21be146be7a375667c9b766d29ab306d64b1f5d1
Anton Taleckij <anton.talecki@gmail.com>
2020-02-09 15:39:51 +0300

    improved enjoyml.layers.FixedPooling2D, added new modules keras.applecations (with U_net function) and keras.metrics (with recall, precision, f1_score, iou metrics)

--- enjoyml/keras/__init__.py
+++ enjoyml/keras/__init__.py
@@ -1,3 +1,5 @@
 from enjoyml.keras import layers
+from enjoyml.keras import metrics
+from enjoyml.keras import applications
 
-__all__ = ['layers']
+__all__ = ['layers', 'metrics', 'applications']
--- enjoyml/keras/layers.py
+++ enjoyml/keras/layers.py
@@ -6,19 +6,9 @@ MAX_MODE = 'max'
 AVERAGE_MODE = 'average'
 AVAILABLE_POOL_MODES = (MAX_MODE, AVERAGE_MODE)
 
-params_to_pooling = {
-    2: {
-        MAX_MODE: layers.MaxPool1D,
-        AVERAGE_MODE: layers.AvgPool1D,
-    },
-    3: {
-        MAX_MODE: layers.MaxPool2D,
-        AVERAGE_MODE: layers.AvgPool2D,
-    },
-    4: {
-        MAX_MODE: layers.MaxPool3D,
-        AVERAGE_MODE: layers.AvgPool3D,
-    },
+pool_mode_to_base_layer = {
+    MAX_MODE: layers.MaxPool2D,
+    AVERAGE_MODE: layers.AvgPool2D,
 }
 
 pool_mode_to_func = {
@@ -36,7 +26,6 @@ class FixedPooling2D(layers.Layer):
         self.pool_mode = pool_mode
         self.iterations = iterations
         self.random_state = random_state
-        self.dimension = None
         self.kernel_size = None
         self.base_pool_layer = None
         super().__init__(**kwargs)
@@ -45,7 +34,7 @@ class FixedPooling2D(layers.Layer):
         input_height, input_width = input_size
         output_height, output_width = output_size
         self.kernel_size = input_height // output_height, input_width // output_width
-        PoolClassLayer = params_to_pooling[self.dimension][self.pool_mode]
+        PoolClassLayer = pool_mode_to_base_layer[self.pool_mode]
         return PoolClassLayer(self.kernel_size, padding='same')
 
     def _random_reduce(self, tensor, row_steps, col_steps, seed_up):
@@ -55,31 +44,33 @@ class FixedPooling2D(layers.Layer):
             if row_steps > 0:
                 i = random.randint(0, tensor.get_shape()[1] - 1)
                 pooled_tensor_row = K.expand_dims(
-                    reduce_func([tensor[:, i, :, :],
-                                 tensor[:, i + 1, :, :]], axis=0),
+                    reduce_func([tensor[:, i, :, :], tensor[:, i + 1, :, :]], axis=0),
                     axis=1
                 )
-                tensor = K.concatenate([tensor[:, :i, :, :],
-                                        pooled_tensor_row,
-                                        tensor[:, (i + 2):, :, :]], axis=1)
+                tensor = K.concatenate([
+                    tensor[:, :i, :, :],
+                    pooled_tensor_row,
+                    tensor[:, (i + 2):, :, :]
+                ], axis=1)
                 row_steps -= 1
             if col_steps > 0:
                 j = random.randint(0, tensor.get_shape()[2] - 1)
                 pooled_tensor_col = K.expand_dims(
-                    reduce_func([tensor[:, :, j, :],
-                                 tensor[:, :, j + 1, :]], axis=0),
+                    reduce_func([tensor[:, :, j, :], tensor[:, :, j + 1, :]], axis=0),
                     axis=2
                 )
-                tensor = K.concatenate([tensor[:, :, :j, :],
-                                       pooled_tensor_col,
-                                       tensor[:, :, (j + 2):, :]], axis=2)
+                tensor = K.concatenate([
+                    tensor[:, :, :j, :],
+                    pooled_tensor_col,
+                    tensor[:, :, (j + 2):, :]
+                ], axis=2)
                 col_steps -= 1
         return tensor
 
     def build(self, input_shape):
-        self.dimension = len(input_shape) - 1
-        if self.dimension < 1 or 3 < self.dimension:
-            raise Exception(f'[{self.__class__.__name__}] Invalid dimension: dimension must be in: [1, 3].')
+        dimension = len(input_shape)
+        if dimension != 4:
+            raise Exception(f'[{self.__class__.__name__}] Invalid dimension: expected 4 dims, get {dimension}.')
         if input_shape[1] < self.output_size[0] or input_shape[2] < self.output_size[1]:
             raise Exception(f'[{self.__class__.__name__}] Input shape must be greater or equal to output size.')
         self.base_pool_layer = self._get_base_pool_layer(input_shape[-3: -1], self.output_size)
@@ -87,20 +78,16 @@ class FixedPooling2D(layers.Layer):
 
     def call(self, tensor, **kwargs):
         base_pooled_tensor = self.base_pool_layer(tensor)
-        row_reduce_steps = base_pooled_tensor.get_shape()[-3] - self.output_size[0]
-        col_reduce_steps = base_pooled_tensor.get_shape()[-2] - self.output_size[1]
-        # result_shape_plh = K.placeholder(shape=[None, self.output_size[0], self.output_size[1],
-        #                                         base_pooled_tensor.get_shape()[-1]])
-        # result = K.zeros_like(result_shape_plh)
-        result = None
+        base_pooled_shape = base_pooled_tensor.get_shape()
+        row_reduce_steps = base_pooled_shape[1] - self.output_size[0]
+        col_reduce_steps = base_pooled_shape[2] - self.output_size[1]
+        result = 0
         for iter_n in range(self.iterations):
             pooled_tensor = K.identity(base_pooled_tensor)
-            pooled_tensor = self._random_reduce(pooled_tensor, seed_up=iter_n,
-                                                row_steps=row_reduce_steps, col_steps=col_reduce_steps)
-            if result is None:
-                result = pooled_tensor
-            else:
-                result += pooled_tensor
+            pooled_tensor = self._random_reduce(
+                pooled_tensor, seed_up=iter_n, row_steps=row_reduce_steps, col_steps=col_reduce_steps
+            )
+            result += pooled_tensor
         return result / self.iterations
 
     def compute_output_shape(self, input_shape):
